e65af0be315f8bd56205368e7e1281fd3fe2a196
mdsumon743 <mdsumonhossan743@gmail.com>
2024-11-19 14:49:34 +0600

    images

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
-import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(TreeGrowthApp());
+  runApp(const TreeGrowthApp());
 }
 
 class TreeGrowthApp extends StatelessWidget {
+  const TreeGrowthApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
@@ -19,92 +21,73 @@ class TreeGrowthScreen extends StatefulWidget {
   const TreeGrowthScreen({super.key});
 
   @override
+  // ignore: library_private_types_in_public_api
   _TreeGrowthScreenState createState() => _TreeGrowthScreenState();
 }
 
 class _TreeGrowthScreenState extends State<TreeGrowthScreen> {
-  List<String> treeImages = [
-    'assets/images/bean.png',
-    'assets/images/bean1.png',
-    'assets/images/bean2.png',
-    'assets/images/bean3.png',
-    'assets/images/bean4.png',
-    'assets/images/bean5.png',
-    'assets/images/bean6.png',
-    'assets/images/bean7.png',
-    'assets/images/bean8.png',
-    'assets/images/bean9.png',
-    'assets/images/bean10.png',
-    'assets/images/bean11.png',
-    'assets/images/bean12.png',
-    'assets/images/bean13.png',
-    'assets/images/bean14.png',
-    'assets/images/bean15.png',
-    'assets/images/bean16.png',
-    'assets/images/bean17.png',
-    'assets/images/bean18.png',
-    'assets/images/bean19.png',
-    'assets/images/bean20.png',
-    'assets/images/bean21.png',
-    'assets/images/bean22.png',
-    'assets/images/bean23.png',
-    'assets/images/bean24.png',
-    'assets/images/bean25.png',
-    'assets/images/bean26.png',
-    'assets/images/bean27.png',
-    'assets/images/bean28.png',
-    'assets/images/bean29.png',
-    'assets/images/bean30.png',
-    'assets/images/bean31.png',
-    'assets/images/bean32.png',
-    'assets/images/bean33.png',
-    'assets/images/bean34.png',
-    'assets/images/bean35.png',
-    'assets/images/bean36.png',
-    'assets/images/bean37.png',
-    'assets/images/bean38.png',
-    'assets/images/bean39.png',
-    'assets/images/bean40.png',
-    'assets/images/bean41.png',
-    'assets/images/bean42.png',
-    'assets/images/bean43.png',
-    'assets/images/bean44.png',
-    'assets/images/bean45.png',
-    'assets/images/bean46.png',
-    'assets/images/bean47.png',
-    'assets/images/bean48.png',
-    'assets/images/bean49.png',
-    'assets/images/bean50.png', 
-    'assets/images/bean51.png',
-    'assets/images/bean52.png',
-    'assets/images/bean53.png',
-  ];
-  int _currentImageIndex = 0;
-  Timer? _timer;
+ List<String> treeImages = [
+    'assets/images/1.png',
+    'assets/images/2.png',
+    'assets/images/3.png',
+    'assets/images/4.png',
+    'assets/images/5.png',
+    'assets/images/6.png',
+    'assets/images/7.png',
+    'assets/images/8.png',
+    'assets/images/9.png',
+    'assets/images/10.png',
+    'assets/images/11.png',
+    'assets/images/12.png',
+    'assets/images/13.png',
+    'assets/images/14.png',
+    'assets/images/15.png',
+    'assets/images/16.png',
+    'assets/images/17.png',
+    'assets/images/18.png',
+    'assets/images/19.png',
+    'assets/images/20.png',
+    'assets/images/21.png',
+    'assets/images/22.png',
+    'assets/images/23.png',
+    'assets/images/24.png',
+    'assets/images/25.png',
+    'assets/images/26.png',
+    'assets/images/27.png',
+    'assets/images/28.png',
+    'assets/images/29.png',
+    'assets/images/30.png',
+    'assets/images/31.png',
+    'assets/images/32.png',
+    'assets/images/33.png',
+    'assets/images/34.png',
+    'assets/images/35.png',
+    'assets/images/36.png',
+    'assets/images/37.png',
+    'assets/images/38.png',
+    'assets/images/39.png',
+    'assets/images/40.png',
+    'assets/images/41.png',
+    'assets/images/42.png',
+    'assets/images/43.png',
+    'assets/images/44.png',
+    'assets/images/45.png',
+    'assets/images/46.png',
+    'assets/images/47.png',
+    'assets/images/48.png',
+    'assets/images/49.png',
+    'assets/images/50.png',
+    'assets/images/51.png',
+    'assets/images/52.png',
+    'assets/images/53.png',
+    'assets/images/54.png',
+    'assets/images/55.png',
+    'assets/images/56.png',
+    'assets/images/57.png',
+    'assets/images/tob.png'
+];
 
-  @override
-  void initState() {
-    super.initState();
-    _startAnimation();
-  }
 
-  @override
-  void dispose() {
-    _timer?.cancel();
-    super.dispose();
-  }
-
-  void _startAnimation() {
-    _timer = Timer.periodic(const Duration(milliseconds: 3000), (timer) {
-      setState(() {
-        if (_currentImageIndex < treeImages.length - 1) {
-          _currentImageIndex++;
-        } else {
-          _timer?.cancel();
-        }
-      });
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -113,37 +96,27 @@ class _TreeGrowthScreenState extends State<TreeGrowthScreen> {
         title: const Text("Tree Growth Animation"),
         centerTitle: true,
       ),
-      body: Column(
+      body:   Column(
         children: [
-          const SizedBox(height: 20),
+           SizedBox(height: 20),
           Expanded(
             child: Center(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                  AnimatedSwitcher(
-                    duration: const Duration(milliseconds: 1000),
-                    transitionBuilder: (Widget child, Animation<double> animation) {
-                      // A combination of scaling and sliding animation for smooth effect
-                      return FadeTransition(
-                        opacity: animation,
-                        
-                        child: child,
-                      );
-                    },
-                    child: Image.asset(
-                      treeImages[_currentImageIndex],
-                      key: ValueKey<int>(_currentImageIndex), // Ensure uniqueness
-                      fit: BoxFit.contain,
-                    ),
-                  ),
+                  Stack(
+                    children: [
+                      Image.asset('assets/images/tob.png')
+                    ],
+                  )
                 ],
               ),
             ),
           ),
-          const SizedBox(height: 20),
+           SizedBox(height: 20),
         ],
       ),
     );
   }
 }
+ 
